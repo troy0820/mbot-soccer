b4da77edd32e84bcdc3f845c5cdd0c3f728a5f72
Linda Nichols <lynnaloo@gmail.com>
2016-02-28 22:44:13 -0500

    commit controller code

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-# mbot-soccer
-mBot SoccerBot
+# Bluetooth controlled mBot SoccerBot
+
+Using the [mBot Bluetooth version](http://www.makeblock.cc/mbot/) and
+[mBot NodeBots](https://github.com/Makeblock-official/mbot_nodebots/blob/master/README.md).
+
+## Getting Started
+
+* Install Node 4
+* Install USB [driver](https://github.com/Makeblock-official/mbot_nodebots/tree/master/driver) for your platform
+
+### Install Dependencies
+
+```
+  git clone https://github.com/EmergingTechnologyAdvisors/mbot-soccer
+  cd mbot-soccer
+  npm i
+```  
+
+### Install Firmware (USB)
+
+* Connect the USB cable
+* Install the USB Firmata:
+
+```
+  interchange install git+https://github.com/Makeblock-official/mbot_nodebots -a uno --firmata=bluetooth
+```
+
+### Install Firmware (Bluetooth)
+
+* Remove the Bluetooth module from the mBot
+* Connect the USB cable
+* Install the Bluetooth Firmata:
+
+```
+  interchange install git+https://github.com/Makeblock-official/mbot_nodebots -a uno --firmata=usb
+```
+
+* Restart the mBot with the Bluetooth module installed again
+* Pair the MakeBlock Bluetooth module (device Bluetooth settings)
+
+## Running the Code
+
+```
+  node eg/controls.js /dev/tty.Makeblock-ELETSPP
+```
+* Control the mBot using the right keyboard keys and spacebar
---
+++ eg/controls.js
@@ -0,0 +1,91 @@
+const keypress = require('keypress');
+const five = require('johnny-five');
+const songs = require('j5-songs');
+
+const board = new five.Board({
+  port: process.argv[2]
+});
+
+const MAX_SPEED = 200;
+const MAX_SPEED_TURNS = 85;
+
+board.on('ready', (err) => {
+  if (err) {
+    console.log('Oops, there was an error:', err);
+    return;
+  }
+
+  const motors = {
+    left: new five.Motor([6, 7]),
+    right: new five.Motor([5, 4])
+  };
+  const piezo = new five.Piezo(8);
+
+  this.repl.inject({
+    motors: motors,
+    piezo: piezo,
+    sonar: sonar
+  });
+
+  console.info('Board connected. Welcome to mBot Controls!');
+  console.log('Control the bot with the right arrow keys, and SPACE to stop.');
+
+  function controls(ch, key) {
+    if ( !key ) { return; }
+
+    if ( key.name === 'q' ) {
+      console.log('Quitting mBot Controls');
+      motors.left.stop();
+      motors.right.stop();
+      process.exit();
+    }
+
+    if ( key.name === 'space' ) {
+      console.log('Stopping');
+      motors.left.stop();
+      motors.right.stop();
+    }
+
+    if ( key.name === 'up' ) {
+      console.log('Going forward');
+      motors.left.rev(MAX_SPEED);
+      motors.right.fwd(MAX_SPEED);
+    }
+
+    if ( key.name === 'down' ) {
+      console.log('Going backward');
+      motors.left.fwd(MAX_SPEED);
+      motors.right.rev(MAX_SPEED);
+    }
+
+    if ( key.name === 'right' ) {
+      console.log('Going right');
+      motors.left.rev(MAX_SPEED_TURNS);
+      motors.right.rev(MAX_SPEED_TURNS);
+    }
+
+    if ( key.name === 'left' ) {
+      console.log('Going left');
+      motors.left.fwd(MAX_SPEED_TURNS);
+      motors.right.fwd(MAX_SPEED_TURNS);
+    }
+
+    if ( key.name === 't' ) {
+      console.log('Turbo Boost!!');
+      motors.left.rev(255);
+      motors.right.fwd(255);
+    }
+
+    if ( key.name === 'r' ) {
+      console.log('You got Rick Rolled!!');
+      piezo.play(songs.load('never-gonna-give-you-up'));
+      motors.left.fwd(255);
+      motors.right.fwd(255);
+    }
+  }
+
+  keypress(process.stdin);
+  process.stdin.on('keypress', controls);
+  process.stdin.setRawMode(true);
+  process.stdin.resume();
+});
